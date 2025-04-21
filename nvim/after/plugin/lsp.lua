4a6ebd13031f7d71beb1da7a3cd953b3de55fc93
aaronx3011 <aadavidh@gmail.com>
2025-04-21 12:16:34 -0400

    new config (arch)

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -20,6 +20,21 @@ lspconfig.clangd.setup {
     capabilities = capabilities,
 }
 
+lspconfig.tsserver.setup {
+    on_attach =  function(client, bufnr)
+-- format on save
+  if client.server_capabilities.documentFormattingProvider then
+    vim.api.nvim_create_autocmd("BufWritePre", {
+      group = vim.api.nvim_create_augroup("Format", { clear = true }),
+      buffer = bufnr,
+      callback = function() vim.lsp.buf.formatting_seq_sync() end
+    })
+  end
+  end,
+  filetypes = {"typescript", "typescriptreact", "typescript.tsx"},
+  cmd = {"typescript-language-server", "--stdio"}
+}
+
 
 -- 3. Autocommands for LSP actions (your existing code, slightly improved):
 vim.api.nvim_create_autocmd('LspAttach', {
